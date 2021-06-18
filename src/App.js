d9c48ef3ed55b4520bbe385fc710322c6ac1ddb8
Ahsan Ahmed <Ahsan.Ahmed.026@gmail.com>
2021-06-18 22:54:34 +0500

    Added Component.
    Add same file into seperated to component base
    1: Add Pre loader
    2: Add component for small perticles
    :100:

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
-// import logo from './logo.svg';
 import './App.css';
 import Api from './Component/Api';
-
 function App() {
   return (
     <div className="App">
---
+++ src/Component/Loading.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from '../logo192.png';
+import '../App.css';
+
+const Loading =() => {
+    return(
+        <>
+        <img src={logo} className="App-logo" alt="" />
+        <h1 className="Load">Loading...</h1>
+        </>
+    )
+}
+export default Loading;
